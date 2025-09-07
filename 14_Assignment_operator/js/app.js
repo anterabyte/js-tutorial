2449073abc40356a48f1d1f04e2990aa6dec3839
anterabyte <dragomon27@gmail.com>
2025-09-07 21:02:37 +0530

    Added new tutorials for Operators (Assignment, Unary, Conditional)

---
+++ 14_Assignment_operator/js/app.js
@@ -0,0 +1,41 @@
+// Assignment Operator
+
+let assign = 32
+
+assign = "Rum"
+
+assign = 34
+
+assign = assign + 2
+
+assign += 3;
+
+assign -= 2;
+
+assign *= 1 ;
+
+assign /= 2 ;
+
+assign %= 3 ;
+
+assign &= 23 ;
+
+assign |= 32 ;
+
+assign ^= 23 ;
+
+assign <<= 1 ;
+
+assign >>= 2 ;
+
+assign >>>= 1 ;
+
+// Chain assign operator
+
+let a= 10 , b =20, c = 30;
+
+a = b = c // 30
+
+// operation run right to left so,
+
+// step 1 , b = c then  a = b; 
